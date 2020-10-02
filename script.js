f7871e1e810068b75a6a0756452e57d0d6394daa
Abhimanyu Sharan <abhimanyu.sharan@gmail.com>
2020-10-03 00:08:18 +0530

    Modified JS function

--- script.js
+++ script.js
@@ -1,27 +1,22 @@
 function action(e){
-    //Removing existing active and showsect classes
-    try {
-        li_existing = document.getElementsByClassName("active")[0];
-        li_existing.classList.remove("active");
-    }
-    try {
-        sect_existing = document.getElementsByClassName("showsect")[0];
-        sect_existing.classList.remove("showsect");
-    }
 
-    //Adding active class to the clicked element's li. [1]=li
-    e.path[1].classList.add("active");
+    //Removing existing active and showsect classes
+    a_existing = document.getElementsByClassName("active")[0];
+    a_existing.classList.remove("active");
+    sect_existing = document.getElementsByClassName("showsect")[0];
+    sect_existing.classList.remove("showsect");
 
     //Adding showsect class to the clicked element's href
-    sect_href = e.target.href;
+    sect_href = e.newURL || e.target.URL;
     sect_id = sect_href.substring(sect_href.indexOf('#') + 1);
     sect_element = document.getElementById(sect_id);
     sect_element.classList.add("showsect");
+
+    //Adding active class to the clicked element's a.
+    href_value = "#"+sect_id;
+    a_element = document.querySelector("[href=" + CSS.escape(href_value) + "]");
+    a_element.classList.add("active");
 }
 
-
-var arr = document.querySelectorAll("#intro li a");
-for (var a of arr){
-    if (a == arr[arr.length - 1]){break;}
-	a.addEventListener("click", action);
-}
+document.body.onload=action;
+window.addEventListener("hashchange", action);
